7fbb8c7f6ad5bed106e16a16c4f30ab2cae4e806
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-08-24 23:27:33 +0300

    Read exif data

--- lib/models/upload_file_info.dart
+++ lib/models/upload_file_info.dart
@@ -1,10 +1,15 @@
+import 'package:nature_photos/controllers/add_photo_controller.dart';
+
 class UploadFileInfo {
   UploadFileInfo({
     required this.fileName,
+    required this.exifData,
   });
   final String fileName;
+  final ExifData exifData;
 
   Map<String, dynamic> toJson() => {
         'fileName': fileName,
+        'exifData': exifData.toJson(),
       };
 }
